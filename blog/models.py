8603dcf0fa4c7218c3d9e35cbb6653825de48b4b
Paul Barajas <paul.barajas@linux.com>
2020-10-31 13:53:02 -0600

    adding models to Comments and creating forms to the models

--- blog/models.py
+++ blog/models.py
@@ -43,3 +43,22 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+class Comment(models.Model):
+    #related_name attribute allows you to name the attribute that
+    #you use for the relationship from the related object back to this one.
+    post = models.ForeignKey(Post,
+                             on_delete=models.CASCADE,
+                             related_name='comments')
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        ordering =  ('created',)
+    def __str__(self):
+        return f'Comment by {self.name} on {self.post}'
+
